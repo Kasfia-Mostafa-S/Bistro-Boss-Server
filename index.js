4c1e5588326d566875a1d2c76e48ff9d81abc165
Kasfia-Mostafa-S <kasfiamostafa.s@gmail.com>
2024-03-30 00:05:53 +0600

    update items

--- index.js
+++ index.js
@@ -138,6 +138,31 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/menu/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await menuCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.patch("/menu/:id", async (req, res) => {
+      const item = req.body;
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          name: item.name,
+          category: item.category,
+          price: item.price,
+          recipe: item.recipe,
+          image: item.image,
+        },
+      };
+      const result = await menuCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
+
+
     // cart collection
     app.get("/carts", async (req, res) => {
       const email = req.query.email;
